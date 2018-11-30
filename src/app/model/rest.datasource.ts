f10af348e0d1067f0ba4bab877f0829862c51c24
tiensinh24 <nguyenphucan2502@gmail.com>
2018-11-30 09:31:08 +0700

    Complete C24

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -1,7 +1,9 @@
 import { InjectionToken, Injectable, Inject } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Product } from "./product.model";
 import { Observable } from "rxjs/Observable";
+import { catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
 
 export const REST_URL = new InjectionToken("rest_url");
 
@@ -14,18 +16,35 @@ export class RestDataSource {
   ) { }
 
   getData(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.url);
+    // return this.http.get<Product[]>(this.url);
+    return this.sendRequest<Product[]>("GET", this.url);
   }
 
   saveProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.url, product);
+    // return this.http.post<Product>(this.url, product);
+    return this.sendRequest<Product>("POST", this.url, product);
   }
 
   updateProduct(product: Product): Observable<Product> {
-    return this.http.put<Product>(`${this.url}/${product.id}`, product);
+    // return this.http.put<Product>(`${this.url}/${product.id}`, product);
+    return this.sendRequest<Product>("PUT", `${this.url}/${product.id}`, product);
   }
 
   deleteProduct(id: number): Observable<Product> {
-    return this.http.delete<Product>(`${this.url}/${id}`);
+    // return this.http.delete<Product>(`${this.url}/${id}`);
+    return this.sendRequest<Product>("DELETE", `${this.url}/${id}`)
+  }
+
+  private sendRequest<T>(verb: string, url: string, body?: Product): Observable<T> {
+
+    let myHeaders = new HttpHeaders();
+    myHeaders = myHeaders.set("Access-Key", "<secret>")
+    myHeaders = myHeaders.set("Application-Name", ["exampleApp", "proAngular"]);
+
+    return this.http.request<T>(verb, url, {
+      body: body,
+      headers: myHeaders
+    }).pipe(catchError((error: Response) =>
+        throwError(`Network Error: ${error.statusText} (${error.status})`)));
   }
 }
